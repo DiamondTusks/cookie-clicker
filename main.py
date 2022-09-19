44c0950264b7323ff3e07525e2fd6d0e1f150d9d
Diamond Tusks <90947391+DiamondTusks@users.noreply.github.com>
2022-09-20 00:23:16 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -6,27 +6,63 @@ import time
 chrome_driver_path = "/Users/me/Documents/development/chromedriver"
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service)
-
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
-cookie = driver.find_element(By.ID, "cookie")
-buy_cursor = driver.find_element(By.ID, "buyCursor")
-buy_grandma = driver.find_element(By.ID, "buyGrandma")
-buy_factory = driver.find_element(By.ID, "buyFactory")
-buy_mine = driver.find_element(By.ID, "buyMine")
-buy_shipment = driver.find_element(By.ID, "buyShipment")
-buy_alchemy_lab = driver.find_element(By.ID, "buyAlchemy lab")
-buy_portal = driver.find_element(By.ID, "buyPortal")
-buy_time_machine = driver.find_element(By.ID, "buyTime machine")
 
-timeout = time.time() + 60*5
-while time.time() < timeout:
+# Get cookie to click on.
+cookie = driver.find_element(By.ID, "cookie")
+
+# Get upgrade item ids.
+items = driver.find_elements(By.CSS_SELECTOR, "#store div")
+item_ids = [item.get_attribute("id") for item in items]
+
+timeout = time.time() + 5
+five_min = time.time() + 60*5
+
+while True:
     cookie.click()
-    if time.time() % 5 == 0:
-        buy_time_machine.click()
-        buy_portal.click()
-        buy_alchemy_lab.click()
-        buy_shipment.click()
-        buy_mine.click()
-        buy_factory.click()
-        buy_grandma.click()
-        buy_cursor.click()
+
+    if time.time() > timeout:
+
+        # Get all upgrade <b> tags.
+        all_prices = driver.find_elements(By.CSS_SELECTOR, "#store b")
+        item_prices = []
+
+        # Convert <b> into an integer price
+        for price in all_prices:
+            element_text = price.text
+            if element_text != "":
+                cost = int(element_text.split("-")[1].strip().replace(",", ""))
+                item_prices.append(cost)
+
+        # Create dictionary of store items and prices
+        cookie_upgrades = {}
+        for n in range(len(item_prices)):
+            cookie_upgrades[item_prices[n]] = item_ids[n]
+
+        # Get current cookie count.
+        money_element = driver.find_element(By.ID, "money").text
+        if "," in money_element:
+            money_element = money_element.replace(",", "")
+        cookie_count = int(money_element)
+
+        # Find upgrades that we can currently afford.
+        affordable_upgrades = {}
+        for cost, id in cookie_upgrades.items():
+            if cookie_count > cost:
+                affordable_upgrades[cost] = id
+
+        # Purchase the most expensive affordable upgrade
+        highest_price_affordable_upgrades = max(affordable_upgrades)
+        print(highest_price_affordable_upgrades)
+        to_purchase_id = affordable_upgrades[highest_price_affordable_upgrades]
+
+        driver.find_element(By.ID, to_purchase_id).click()
+
+        # Add another 5 seconds until the next check.
+        timeout = time.time() + 5
+
+    # After 5 minutes stop the bot and check cookies per second count.
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element(By.ID, "cps").text
+        print(cookie_per_s)
+        break
